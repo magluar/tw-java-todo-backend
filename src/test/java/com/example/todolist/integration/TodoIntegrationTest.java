2accd23a2a6e09041b3783c0d9fa13142bfcee6d
magluar <aryann.magluyan@oocl.com>
2021-08-19 15:52:38 +0800

    [todo][magluar] feat: should_return_updated_todo_item_when_call_updateTodoItem_api

--- src/test/java/com/example/todolist/integration/TodoIntegrationTest.java
+++ src/test/java/com/example/todolist/integration/TodoIntegrationTest.java
@@ -2,9 +2,7 @@ package com.example.todolist.integration;
 
 import com.example.todolist.model.Todo;
 import com.example.todolist.repository.TodoListRepository;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.AfterEach;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -63,5 +63,26 @@ public class TodoIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.text").value("random text haha"))
                 .andExpect(jsonPath("$.done").value("false"));
+    }
+
+    @Test
+    void should_return_updated_todo_item_when_call_updateTodoItem_api() throws Exception {
+        //given
+        final Todo todo = new Todo("abcd1234", false);
+        String updatedTodoItem = "{\n" +
+                "    \"done\": true\n" +
+                "}";
+
+        //when
+
+        //then
+        int id = todoListRepository.save(todo).getId();
+        mockMvc.perform(MockMvcRequestBuilders.put("/todos/{id}", id)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(updatedTodoItem))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.text").value("abcd1234"))
+                .andExpect(jsonPath("$.done").value("true"));
     }
 }
