0f2c3094d102f6a64a3c2ea9c919e2936edf8e23
magluar <aryann.magluyan@oocl.com>
2021-08-19 14:22:28 +0800

    [todo][magluar] feat: add CorsGlobalConfiguration to avoid CORS policy error

--- src/main/java/com/example/todolist/controller/TodoListController.java
+++ src/main/java/com/example/todolist/controller/TodoListController.java
@@ -4,12 +4,14 @@ import com.example.todolist.model.Todo;
 import com.example.todolist.repository.TodoListRepository;
 import com.example.todolist.service.TodoListService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/todos")
 public class TodoListController {
